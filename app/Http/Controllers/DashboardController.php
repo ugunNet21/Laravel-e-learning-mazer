b905d43b86eb18447156a811f7e0181269d54ed6
Gugun Gunawan <gugun.net21@gmail.com>
2024-03-16 10:41:34 +0700

    change route to dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         return view('backend.dashboard');
