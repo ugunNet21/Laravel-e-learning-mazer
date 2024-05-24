7e560c40963fe2e76fe8e26ebdbf80f936978ac8
Gugun Gunawan <gugun.net21@gmail.com>
2024-05-24 10:45:15 +0700

    -

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- favicon-->
+    <link rel="shortcut icon" href="{{ asset('frontend/favicon.svg') }}" type="image/svg+xml">
+
     <title>@yield('title', 'Home')</title>
     @include('frontend.partials.styles')
 </head>
--- resources/views/frontend/partials/styles.blade.php
+++ resources/views/frontend/partials/styles.blade.php
@@ -1,6 +1,3 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- favicon-->
 <link rel="shortcut icon" href="{{ asset('frontend/favicon.svg') }}" type="image/svg+xml">
